2996da004d3b096886f30cf08b84aa894eee5c87
mtleiva <migueltomasleiva@gmail.com>
2023-12-25 16:24:20 -0300

    Agrego Microsoft.EntityFrameworkCore.Design

---
+++ Sources/IntuitClientes.Domain/Context/DbContext.cs
@@ -0,0 +1,26 @@
+﻿using IntuitClientes.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntuitClientes.Domain.Context
+{
+    public class DBContext : DbContext
+    {
+        public DBContext() { }
+
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+        public DbSet<Client> Clients { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = "Server=LOCALHOST\\SQLEXPRESS;Database=IntuitClients;TrustServerCertificate=True;Trusted_Connection=True;MultipleActiveResultSets=true";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
