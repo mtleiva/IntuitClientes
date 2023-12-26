2eccf67c31ce8f63297733587bcf7f6cb54b98aa
mtleiva <migueltomasleiva@gmail.com>
2023-12-26 01:02:35 -0300

    Merge develop

--- Sources/IntuitClientes.CrossCutting/obj/Debug/net8.0/IntuitClientes.CrossCutting.AssemblyInfo.cs
+++ Sources/IntuitClientes.CrossCutting/obj/Debug/net8.0/IntuitClientes.CrossCutting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntuitClientes.CrossCutting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9408cfa2483509c860e6d2190cb35140b9d37667")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntuitClientes.CrossCutting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntuitClientes.CrossCutting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
